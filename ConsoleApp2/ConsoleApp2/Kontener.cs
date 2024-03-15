f0d77991586db6022107fab51d8892e3e16838b8
s28517 <s28517@pjwstk.edu.pl>
2024-03-15 13:32:41 +0100

    konterener abstract

---
+++ ConsoleApp2/ConsoleApp2/Kontener.cs
@@ -0,0 +1,45 @@
+﻿namespace ConsoleApp2;
+
+public abstract class Kontener
+{
+    protected Kontener(int masaLadunku, int wysokosc, int wagaWlasna, int glebokosc, NumerSeryjny numerSeryjny, int maksymalnaLadownosc)
+    {
+        this.masaLadunku = masaLadunku;
+        this.wysokosc = wysokosc;
+        this.wagaWlasna = wagaWlasna;
+        this.glebokosc = glebokosc;
+        this.numerSeryjny = numerSeryjny;
+        this.maksymalnaLadownosc = maksymalnaLadownosc;
+    }
+
+    protected int masaLadunku { get; set; }
+    protected int wysokosc { get; set; }
+    protected int wagaWlasna { get; set; }
+    protected int glebokosc { get; set; }
+    protected NumerSeryjny numerSeryjny { get; set; }
+    protected int maksymalnaLadownosc { get; set; }
+
+
+    public void Unload()
+    {
+        masaLadunku = 0;
+    }
+
+    public void Load(int waga)
+    {
+        masaLadunku = waga;
+        checkWeight();
+    }
+
+    public void checkWeight()
+    {
+        if (masaLadunku > maksymalnaLadownosc)
+        {
+            Console.WriteLine("Wyrzuci blad!");
+        }
+    }
+    
+    
+
+
+}
---
+++ ConsoleApp2/ConsoleApp2/NumerSeryjny.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp2;
+
+public class NumerSeryjny
+{
+    private string first;
+    private string second;
+    private static int number = 1;
+    private int numer;
+
+    public NumerSeryjny(string first, string second)
+    {
+        this.first = first;
+        this.second = second;
+        number++;
+        numer = number;
+    }
+
+    public string getNumerSeryjny()
+    {
+        return first + "-" + second + "-" + numer;
+    }
+
+}
